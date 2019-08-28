7305e1c6261e9aeda27a52ea3f573a5f9edb0e89
Davide Garberi <dade.garberi@gmail.com>
2019-08-28 22:16:15 +0200

    power: Import xiaomi longcheer edits
    * Cleaned up from xiaomi lavender changes and KERNEL_CUSTOM_FACTORY
    * Also cleaned up xiaomi's coding style
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>
    Change-Id: Ife3f2e7e643b2a8176ed9b407683f97b84f4fa85
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- drivers/power/supply/qcom/fg-memif.c
+++ drivers/power/supply/qcom/fg-memif.c
@@ -746,6 +746,7 @@ out:
 	return rc;
 }
 
+#ifdef CONFIG_MACH_XIAOMI_TULIP
 int fg_dma_mem_req(struct fg_chip *chip, bool request)
 {
 	int ret, rc = 0, retry_count  = RETRY_COUNT;
@@ -778,7 +779,7 @@ int fg_dma_mem_req(struct fg_chip *chip, bool request)
 				break;
 			msleep(20);
 		}
-		if ((retry_count < 0) && !(val & MEM_GNT_BIT)) {
+		if (!retry_count && !(val & MEM_GNT_BIT)) {
 			pr_err("failed to get memory access\n");
 			rc = -ETIMEDOUT;
 			goto release_mem;
@@ -803,6 +804,7 @@ release_mem:
 
 	return rc;
 }
+#endif
 
 int fg_ima_init(struct fg_chip *chip)
 {
